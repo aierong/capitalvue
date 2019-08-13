606272fca35f114309c5df7b05ec62fede40125c
aierong <aierong@126.com>
2019-08-13 20:39:23 +0800

    home

---
+++ src/common/constant.js
@@ -0,0 +1,32 @@
+/*
+ 作者: chenghao
+ 功能: 定义一些常量
+ */
+
+//token名称
+// import createPersistedState from "vuex-persistedstate";
+
+export const usertokenname = 'MyWxNewToken'
+
+//bmob  单次API返回数据大小进行限制  204800  bytes
+export const bmobapidatamax = 204800;
+
+
+
+export const MyVuePersistedName = {
+    LoginUserId : 'MyWXLoginUserId'
+}
+
+export const CookieExpires = 7;
+
+//头像列表
+export const avatariconlist = [
+
+    'touxiang4' ,
+    'touxiang3' ,
+    'touxiang2' ,
+    'touxiang1' ,
+
+];
+
+
---
+++ src/common/util/util.js
@@ -0,0 +1,17 @@
+/*
+ 作者: chenghao
+ 功能: js脚本
+ 定义一些公用方法
+ */
+
+// function geticonallname ( val ) {
+//     if ( !val ) {
+//         return ''
+//     }
+//
+//     return '#icon-' + val;
+// }
+
+export {
+    // geticonallname
+}
