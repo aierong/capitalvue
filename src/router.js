b5afde3cce57c9f5b1dc1b226ba8932f2919b398
aierong <aierong@126.com>
2019-08-24 15:04:47 +0800

    company

--- src/router.js
+++ src/router.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
 
+import * as constant from '@/common/constant.js'
+import * as Cookies from 'js-cookie'
+
 Vue.use( Router )
 
-export default new Router( {
+const router = new Router( {
     mode : 'history' ,
     base : process.env.BASE_URL ,
     routes : [
@@ -48,3 +51,31 @@ export default new Router( {
         } ,
     ]
 } )
+
+router.beforeEach( ( to , from , next ) => {
+
+    if ( to.path == '/login' || to.path == '/register' ) {
+
+        next();
+    }
+    else {
+
+        //let data = Cookies.getJSON( constant.MyVuePersistedName.LoginInfoName );
+        //是存的一个对象,这里我们取对象
+        let data = Cookies.getJSON( constant.MyVuePersistedName.LoginUserId );
+
+        // console.log( 'data' , data )
+        //let isLogin = data ? true : false;
+        let isLogin = false;
+        // console.log( 'isLogin' , isLogin )
+
+        if ( data && data.loginuserid ) {
+            isLogin = true;
+        }
+
+        //没有登录，就转向登录页
+        isLogin ? next() : next( '/login' );
+    }
+} );
+
+export default router
