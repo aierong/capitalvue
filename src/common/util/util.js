14adcfabc7bff62a6dd4d923388a86e10c513677
aierong <aierong@126.com>
2019-08-26 15:50:18 +0800

    company

--- src/common/util/util.js
+++ src/common/util/util.js
@@ -4,6 +4,13 @@
  定义一些公用方法
  */
 
+import Mock from "mockjs";
+
+/**
+ *
+ * @param val
+ * @returns {string}
+ */
 export function geticonallname ( val ) {
     if ( !val ) {
         return ''
@@ -12,4 +19,12 @@ export function geticonallname ( val ) {
     return '#icon-' + val;
 }
 
+/**
+ * 得随机中文名字
+ */
+export function getcname () {
+    let cname = Mock.mock( "@cname()" );
+
+    return cname;
+}
 
