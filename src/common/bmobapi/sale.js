3d41401219238a44708c7b021dbc6cc61cee3bc1
aierong <aierong@126.com>
2019-08-28 17:24:30 +0800

    company

--- src/common/bmobapi/sale.js
+++ src/common/bmobapi/sale.js
@@ -15,3 +15,35 @@ import dayjs from 'dayjs'
 const tableName = 'sale';
 
 
+/**
+ * 是存在单号
+ * @param 单号
+ * @returns {Promise<unknown>}
+ */
+export function isexistsnos ( nos ) {
+    return new Promise( ( resolve , reject ) => {
+        const query = Bmob.Query( tableName );
+        query.equalTo( "nos" , "==" , nos );
+
+        query.find().then( res => {
+            //返回的是数组,没有找到就是空数组
+            //console.log( res )
+
+            if ( res != null && res.length > 0 ) {
+                resolve( {
+                    //是存在
+                    isexists : true ,
+                    data : res[ 0 ]
+                } );
+            }
+            else {
+                resolve( {
+                    isexists : false ,
+                    data : null
+                } );
+            }
+        } );
+    } );
+}
+
+
