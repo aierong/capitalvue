26140e47acd0bdc991e08692c8c4576cfd3584ab
aierong <aierong@126.com>
2019-09-02 09:54:09 +0800

    company

---
+++ src/components/detail/nosstep.vue
@@ -0,0 +1,51 @@
+<!--
+作者:chenghao
+Date: 2019/9/2
+Time: 9:53
+功能:
+-->
+
+<!-- html代码片段 -->
+<template>
+
+    <div>
+        nosstep
+    </div>
+
+</template>
+
+<!-- js脚本代码片段 -->
+<script>
+    export default {
+        name : "nosstep" ,
+        //数据模型
+        data () {
+            return {
+                msg : ''
+            }
+        } ,
+        //方法
+        methods : {
+            //methodsname() {
+            //代码搞这里
+            //},
+
+        } ,
+        //计算属性
+        computed : {
+            //name() {
+            //代码搞这里
+            //return this.data;
+            //}
+        } ,
+        //生命周期(mounted)
+        mounted () {
+
+        } ,
+    }
+</script>
+
+<!-- 样式代码片段  scoped -->
+<style scoped
+       src="./nosstep.css">
+</style>
