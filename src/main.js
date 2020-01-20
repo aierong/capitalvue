5691b554a01209adcc724397d974eff5fe8e752a
aierong <aierong@126.com>
2020-01-20 13:43:08 +0800

    company

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import App from './App.vue'
 import store from '@/store/store.js'
 import router from './router'
 
-import Bmob from "hydrogen-js-sdk";
+// import Bmob from "hydrogen-js-sdk";
+
 // 初始化
-Bmob.initialize( "5fa6c3b28a9e4494aaa1c140bfc0e5b0" , "59011ef8242bbe9eb364ffa6fe414360" );
+// 旧版本的： Bmob.initialize("你的Application ID", "你的REST API Key");
+// Bmob.initialize( "5fa6c3b28a9e4494aaa1c140bfc0e5b0" , "59011ef8242bbe9eb364ffa6fe414360" );
+
+import Bmob from "hydrogen-js-sdk";
+//          Bmob.initialize("你的Secret Key", "你的API 安全码");
+//初始化 新版本(2.0以上版本):   Bmob.initialize("你的Secret Key", "你的API 安全码");
+// API 安全码: 在应用功能设置，安全验证，API安全码自己进行设置
+Bmob.initialize( "6c5cee55bfe8e3b1" , "123abc" );
 
 // 挂载到全局使用
 Vue.prototype.$Bmob = Bmob
